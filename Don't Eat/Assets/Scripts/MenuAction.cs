40aa39e07a10123089613a8ac700de9fb8d5c9f8
Aditya Dwicahyo <adityadwicahyo21@gmail.com>
2018-05-12 12:06:13 +0700

    Menambahkan scene start dan gameover.

---
+++ Don't Eat/Assets/Scripts/MenuAction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAction : MonoBehaviour {
+
+    public void MENU_ACTION_GotoPage(string sceneName){
+        Application.LoadLevel(sceneName);
+    }
+
+}
