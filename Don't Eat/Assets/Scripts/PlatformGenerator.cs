e6f9a922889206d5d1b1ac6c122e06abe7ff186f
Aditya Dwicahyo <adityadwicahyo21@gmail.com>
2018-05-12 11:28:57 +0700

    Merge branch 'script_only'
    Menambahkan platform.

---
+++ Don't Eat/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+	
+	public GameObject thePlatform;
+	public Transform generationPoint;
+	public float distanceBetween;
+
+	private float platformWidth;
+
+	public ObjectPooler[] theObjectPools;
+
+	// public GameObject[] thePlatforms;
+	private int platformSelector;
+
+	// Use this for initialization
+	void Start () {
+		platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x < generationPoint.position.x){
+			transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+			platformSelector = Random.Range(0, theObjectPools.Length);
+
+			// Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
+			
+			GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject();
+
+			newPlatform.transform.position = transform.position;
+			newPlatform.transform.rotation = transform.rotation;
+			newPlatform.SetActive(true);
+		}
+	}
+}
