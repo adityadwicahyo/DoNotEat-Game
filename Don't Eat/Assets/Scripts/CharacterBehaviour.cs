13c7098e039b0b91186bf9d8c6377464e70a4887
Aditya Dwicahyo <adityadwicahyo21@gmail.com>
2018-05-12 11:46:51 +0700

    Masukin punya Yoga ke Master

---
+++ Don't Eat/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterBehaviour : MonoBehaviour {
+
+	// character variables
+	Rigidbody2D characterRB;
+
+	public Slider fatIndicator;
+
+	// public variables
+	public float fatPoint;
+	public float characterSpeed;
+	public float speedPerFat;
+
+	// Use this for initialization
+	void Start () {
+		characterRB = this.gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void addFat (float amount) {
+		fatPoint += amount;
+		fatIndicator.value = fatPoint;
+		characterSpeed -= Mathf.Abs (fatPoint) * speedPerFat;
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Enemy") {
+			// Game Over
+		}
+	}
+}
---
+++ Don't Eat/Assets/Scripts/FoodBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBehaviour : MonoBehaviour {
+
+	// public variables
+	public float foodAmount;
+	public float hiddenHealth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hiddenHealth == 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Player") {
+			CharacterBehaviour theCharacter = other.gameObject.GetComponent<CharacterBehaviour> ();
+			theCharacter.addFat (foodAmount);
+			Destroy (gameObject);
+		}
+	}
+
+	void OnMouseOver () {
+		if (Input.GetMouseButtonDown (0) == true) {
+			hiddenHealth -= 1;
+		}
+	}
+}
