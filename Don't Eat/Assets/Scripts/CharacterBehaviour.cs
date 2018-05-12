8abf3cc2f03d65a61bec1ba540c7717afc85672a
Unknown <ygsamudra@gmail.com>
2018-05-12 10:19:15 +0700

    Script karakter dan food

---
+++ Don't Eat/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour {
+
+	// public variables
+	public float fatPoint;
+	public float characterSpeed;
+	public float speedPerFat;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		characterSpeed -= Mathf.Abs (fatPoint) * speedPerFat;
+	}
+
+	public void addFat (float amount)
+	{
+		fatPoint += amount;
+	}
+}
