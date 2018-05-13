a408616ec3abe3237299f9b1a7c54e8d45f063a8
Aditya Dwicahyo <adityadwicahyo21@gmail.com>
2018-05-13 19:44:23 +0700

    Sebelum presentasi.

--- Don't Eat/Assets/Scripts/CharacterBehaviour.cs
+++ Don't Eat/Assets/Scripts/CharacterBehaviour.cs
@@ -2,38 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CharacterBehaviour : MonoBehaviour {
 
 	// character variables
 	Rigidbody2D characterRB;
+	//Animator charAnim;
 
 	public Slider fatIndicator;
 
 	// public variables
 	public float fatPoint;
-	public float characterSpeed;
+	public float maxSpeed;
 	public float speedPerFat;
+	public float hungerTimePerSecond;
+
+	float nextHunger;
+	float nowSpeed;
+	float timeLimit = 10.0f;
+
 
 	// Use this for initialization
 	void Start () {
-		characterRB = this.gameObject.GetComponent<Rigidbody2D> ();
+		characterRB = GetComponent<Rigidbody2D> ();
+		//charAnim = GetComponent<Animator> ();
+
+		nextHunger = hungerTimePerSecond;
+		nowSpeed = maxSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Time.time > nextHunger) {
+			nextHunger += hungerTimePerSecond;
+			fatPoint -= 1;
+		}
+
+		if(Time.time >= timeLimit)
+		{
+      	// Add 1 to the moveSpeed...
+			maxSpeed += 0.5f;
+			nowSpeed += 0.5f;
+      	// make it wait another 30 mins...
+			timeLimit += 10.0f;
+		}
+	}
+
+	void FixedUpdate () {
+		characterRB.velocity = new Vector2(nowSpeed, characterRB.velocity.y);
+		nowSpeed = maxSpeed - (Mathf.Abs (fatPoint) * speedPerFat);
+		fatIndicator.value = fatPoint;
 	}
 
 	public void addFat (float amount) {
 		fatPoint += amount;
-		fatIndicator.value = fatPoint;
-		characterSpeed -= Mathf.Abs (fatPoint) * speedPerFat;
+		
 	}
 
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag == "Enemy") {
 			// Game Over
+			Destroy (gameObject);
+			SceneManager.LoadScene("gameover", LoadSceneMode.Additive);
 		}
 	}
 }
---
+++ Don't Eat/Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour {
+
+	public float moveSpeed;
+	private float timeLimit = 10.0f;
+
+
+	private Rigidbody2D myRigidbody;
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);
+
+		if(Time.time >= timeLimit)
+		{
+      	// Add 1 to the moveSpeed...
+			moveSpeed += 0.5f;
+      	// make it wait another 30 mins...
+			timeLimit += 10.0f;
+		}
+	}
+}
